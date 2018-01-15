df565edcbc0990a3eb2035d17359f692c6f47fd8
MoronixProduct3 <moronicproduct3@gmail.com>
2018-01-14 19:57:44 -0500

    Add project files.

---
+++ photo-preprocessor-admpf310f/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace photo_preprocessor_admpf310f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
